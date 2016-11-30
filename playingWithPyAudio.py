58833c76a817825dfbf94c86a0b6f94f4f24abd3
Rishabh Chatterjee <rishabh.chatterjee98@gmail.com>
2016-11-30 13:51:10 -0500

    Add files via upload

---
+++ playingWithPyAudio.py
@@ -0,0 +1,32 @@
+import pyaudio
+import wave
+import time
+import random
+
+def play(wave_filename):
+    """
+        Plays a WAV file
+    """
+    wf = wave.open(wave_filename, 'rb')
+    p = pyaudio.PyAudio()
+
+    def callback(in_data, frame_count, time_info, status):
+        data = wf.readframes(frame_count)
+        return (data, pyaudio.paContinue)
+
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True,
+                    stream_callback=callback)
+
+    stream.start_stream()
+
+    while stream.is_active():
+        time.sleep(0.0)
+
+    stream.stop_stream()
+    stream.close()
+    wf.close()
+
+    p.terminate()
