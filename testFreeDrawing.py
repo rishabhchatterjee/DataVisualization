cc475e6a60dc8ea6fd939feec0dc3047566d466e
Rishabh Chatterjee <rishabh.chatterjee98@gmail.com>
2016-12-03 23:09:00 -0500

    Add files via upload

--- testFreeDrawing.py
+++ testFreeDrawing.py
@@ -1,9 +1,17 @@
 import turtle
+import random
 
 
 def freeDraw():
-    
-    turtle.shape('circle')
+
+    def setup():
+        turtle.shape('turtle')
+        turtle.penup()
+        turtle.setpos(-125,200)
+        turtle.pendown()
+        turtle.write("Click 'c' to change turtle colors!", font = 'Arial 20 bold')
+        turtle.penup()
+        turtle.setpos(0,0)
 
     def mouseClick(event):
         turtle.penup()
@@ -19,15 +27,25 @@ def freeDraw():
     def reset(event):
         turtle.clear()
 
+    count = 0
+    def changeColor(event):
+        nonlocal count 
+        count += 1
+        colors = ['black','red','dark green','blue','orange','brown']
+        turtle.color(colors[count%6])
+
+
     turtle.reset()
     turtle.speed(0)
 
     c=turtle.getcanvas()
 
+    setup()
     c.bind("<Button-1>", mouseClick)
     c.bind("<B1-Motion>", draw)
     c.bind("<ButtonRelease-1>", release)
     c.bind("<Escape>",reset)
+    c.bind("<c>", changeColor)
 
 
     s=turtle.Screen()
