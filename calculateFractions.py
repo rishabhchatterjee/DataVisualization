01b639b7b3254916ca2fb70eb9f9c20a86ad9b4f
Rishabh Chatterjee <rishabh.chatterjee98@gmail.com>
2016-12-06 21:00:35 -0500

    Add files via upload

--- calculateFractions.py
+++ calculateFractions.py
@@ -1,10 +1,6 @@
 import math, random
 
-################################################################################
-#                GET LARGER CALL STACK TO CALC UPTO MANY DIGITS                #
-################################################################################
-
-def callWithLargeStack(f,*args):   # Course Notes
+def callWithLargeStack(f,*args):            ## Taken from course notes ##
     import sys
     import threading
     threading.stack_size(2**27)  # 64MB stack
@@ -19,29 +15,29 @@ def callWithLargeStack(f,*args):   # Course Notes
     return resultWrapper[0]
 
 ################################################################################
-#                CALCULATE UPTO N DIGITS AFTER . FOR RATIONAL NUMBERS          #
+#                CALCULATE UPTO N DIGITS AFTER DECIMAL POINT                   #
 ################################################################################
 
 def division(divident, divisor, digits = 10, count = 0, ans =[]):
-    if(count >= digits):
+    if(count >= digits):                
         return []
     else:
-        remainder = divident % divisor
+        remainder = divident % divisor      # method of long division
         quotient = divident // divisor
         if(quotient >= 10):
-            ans.append((quotient // 10))
+            ans.append((quotient // 10))    
             ans.append((quotient % 10))
         else:
             ans.append((quotient))
-        nextDivident = remainder*10
+        nextDivident = remainder*10         # 'getting the zero down'
         return  ans + division(nextDivident, divisor, digits, count + 1, ans=[])
 
 ################################################################################
 #                 GET RID OF TERMINATING 0'S LEAVING ONE 0                     #
 ################################################################################
 
-def endingZeroesTerminate(L):
-    zeroCount = 0
+def endingZeroesTerminate(L):           # tells when fraction is terminating
+    zeroCount = 0                       # and stops calculating accordingly
     if(0 in L):
         first = L.index(0)
         remaining  = len(L) - first 
--- fractions.py
+++ fractions.py
@@ -2,35 +2,36 @@ import string, math
 from calculateFractions import *
 from tkinter import simpledialog
 
+class Fraction(object):
+
+    def __init__(self):
+        self.num = simpledialog.askstring(" ", 'Enter numerator of fraction')
+        for digit in self.num:              # checks valid input
+            if digit not in '0123456789':
+                self.num = simpledialog.askstring(" ",'Enter valid integer please!')
+
+        self.den = simpledialog.askstring(" ", 'Enter denominator of fraction')
+        for digit in self.den:
+            if digit not in '0123456789':
+                self.den = simpledialog.askstring(" ", 'Enter valid integer please!')
+        while(self.den == '0'):             # checks non zero denominator
+            self.den = int(simpledialog.askstring(" ", 'Please Enter non 0 denominator'))
+        
+        self.num = int(self.num)
+        self.den = int(self.den)
+        self.digits = (simpledialog.askstring(" ", 'Enter number of decimal places'))
+        for digit in self.digits:
+            if digit not in '0123456789':
+                self.digits = simpledialog.askstring(" ", 'Enter valid integer please!')
+        self.digits = int(self.digits)
+        if(self.digits == 0):
+            self.digits = int(simpledialog.askstring(" ", 'Enter non zero integer please!'))
+
+    def __repr__(self):
+        return '\nEntered Fraction is %d / %d' %(self.num, self.den)
 
 def getInputs():
     A = Fraction()
     ans = endingZeroesTerminate((callWithLargeStack(division,A.num,A.den,A.digits)))   
     print(ans)
     return ans
-
-class Fraction(object):
-
-    def __init__(self):
-        self.num = simpledialog.askstring(" ", 'Enter numerator of fraction')
-        for digit in self.num:
-            if digit not in '0123456789':
-                self.num = input('\nEnter valid integer please!\n')
-        self.den = simpledialog.askstring(" ", 'Enter denominator of fraction')
-        for digit in self.den:
-            if digit not in '0123456789':
-                self.den = simpledialog.askstring(" ", 'Enter valid integer please!')
-        while(self.den == '0'):
-            self.den = int(simpledialog.askstring(" ", 'Please Enter non 0 denominator'))
-        
-        self.num = int(self.num)
-        self.den = int(self.den)
-        self.digits = int(simpledialog.askstring(" ", 'Enter number of decimal places'))
-
-    def __repr__(self):
-        return '\nEntered Fraction is %d / %d' %(self.num, self.den)
-
-# A = Fraction()
-# ans = endingZeroesTerminate((callWithLargeStack(division,A.num,A.den,A.digits)))   
-# print(ans)
-#getInputs()
